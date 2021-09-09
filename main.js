3c458a8d29d83a879bbc4aa87271a6b7d044a694
Yash Patel <yashapatel17@gmail.com>
2021-09-09 16:07:09 -0400

    Demo website Completed with minimum functinality. Also Implemented Dark/light theme

--- main.js
+++ main.js
@@ -1,83 +1,121 @@
- /*===== MENU SHOW =====*/ 
-const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
 
-    // showing show-menu class if toggle and nav exsist.
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
-            nav.classList.toggle('show-menu')
-        })
-    }
-}
-showMenu('nav-toggle','nav-menu')
+  // showing show-menu class if toggle and nav exsist.
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show-menu");
+    });
+  }
+};
+showMenu("nav-toggle", "nav-menu");
 
 // Remove menu mobile
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll(".nav__link");
 
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    // when we click on each nav__link we remove the show-menu class.
-    navMenu.classList.remove('show-menu')
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  // when we click on each nav__link we remove the show-menu class.
+  navMenu.classList.remove("show-menu");
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
-
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*===== SCROLL SECTIONS ACTIVE LINK =====*/
-const sections = document.querySelectorAll('section[id]')
+const sections = document.querySelectorAll("section[id]");
 
-window.addEventListener('scroll', scrollActive)
+window.addEventListener("scroll", scrollActive);
 
-function scrollActive(){
-    const scrollY = window.pageYOffset
+function scrollActive() {
+  const scrollY = window.pageYOffset;
 
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-        }
-    })
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
 }
 
 // change background header
 
-function scrollHeader(){
-    const nav = document.getElementById('header');
-    // When the scrolll is greater the 200 viewport height add the scroll header class to the header tag.
-    if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
-
+function scrollHeader() {
+  const nav = document.getElementById("header");
+  // When the scrolll is greater the 200 viewport height add the scroll header class to the header tag.
+  if (this.scrollY >= 200) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
 }
 
-window.addEventListener('scroll', scrollHeader)
+window.addEventListener("scroll", scrollHeader);
 
 // Show scroll top
-function scrollTop(){
-    const scrollTop = document.getElementById('scroll-top');
-    // When the scrolll is greater the 560 viewport height add the scroll header class to the header tag.
-    if(this.scrollY >= 560) scrollTop.classList.add('scroll-top'); else scrollTop.classList.remove('scroll-top')
-
+function scrollTop() {
+  const scrollTop = document.getElementById("scroll-top");
+  // When the scrolll is greater the 560 viewport height add the scroll header class to the header tag.
+  if (this.scrollY >= 560) scrollTop.classList.add("scroll-top");
+  else scrollTop.classList.remove("scroll-top");
 }
 
-window.addEventListener('scroll', scrollTop)
-
+window.addEventListener("scroll", scrollTop);
 
 // Dark light theme
 
-const themeButton = document.getElementById('theme-button');
-const darkTheme = 'dark-theme';
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "bx-sun";
 
+// previously selected topic
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
 
+// we obtain the current theme that the interface has by validiting the theme
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "bx-moon" : "bx-sun";
 
-
-
-
+// We validtae if the user previously chose a topic
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "bxs-moon" ? "add" : "remove"](
+    iconTheme
+  );
+}
 
 // Activate and deactivate the theme through the button
-themeButton.addEventListener('click',()=>{
-    document.body.classList.toggle(darkTheme)
-})
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+
+  localStorage, setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
+
+// ScrollReveal
+
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 2000,
+  reset: true,
+});
+
+
+sr.reveal(
+  ".home__data, .home__img, .about__data, .about__img, .services__content, .projects__content,.contact__data, .contact__button, .footer__content ",
+  {
+    interval: 100,
+  }
+);
